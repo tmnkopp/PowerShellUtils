276bf04454221397296521d770d07726c06c9ee1
Tim Kopp <timkopp@gmail.com>
2021-03-25 22:47:49 -0400

    added ps for git commands

--- GIT.ps1
+++ GIT.ps1
@@ -1,55 +1,26 @@
-﻿
-cd C:\users\tim\source\repos\SledgeOMatic;
-git add .; git commit -m "refacted compiler "; git push;
- 
-cd C:\users\tim\source\repos\Compiler
-git add .; git commit -m "refacted compiler ";git push;
-
-cd C:\users\tim\source\repos\ConsoleApp1
-git add .; git commit -m "refacted code gen";git push; 
-
-cd C:\users\tim\source\repos\somuing
-git add .; git commit -m "added support for code gen";git push;
- 
-cd C:\users\tim\source\repos\SOM-API
-git add .; git commit -m "added code gen supporting model";git push;
-
-cd C:\users\tim\source\repos\SOMData
-git add .; git commit -m "added code gen";git push;
-
-cd C:\users\tim\source\repos\scraper
-git add .; git commit -m "added scaf";git push;
-
-
-
-
-
-cd C:\users\tim\source\repos\JiraPortal
-git add .; git commit -m "updated formatting";git push;
-
-cd C:\users\tim\source\repos\JiraFormatter
-git add .; git commit -m "updated formatting";git push;
-
-cd C:\users\tim\source\repos\JiraCore
-git add .; git commit -m "updated formatting";git push;
-
-cd C:\users\tim\source\repos\AssistWith
-git add .; git commit -m "added jobs";git push;
-
-cd 'C:\Users\Tim\Documents\SQL Server Management Studio\SQL_PROJECTS\SQL_PROJECTS'
-git add .; git commit -m "refacted sql metrics";  git push;
-
-
-cd 'D:\PS'
-git add .; git commit -m "updated SVN commit commands   ";git push;
-
-
-
+﻿ 
 cd 'D:\PS'
 git remote set-url origin   https://github.com/tmnkopp/PowerShellUtils.git
 git push --set-upstream origin master
 git remote -v
 git status
 git add .; git commit -m "added ps for git commands";git push;
-
  
+cd C:\users\tim\source\repos\JiraPortal
+git add .; git commit -m "updated formatting";git push; 
+cd C:\users\tim\source\repos\JiraFormatter
+git add .; git commit -m "updated formatting";git push; 
+cd C:\users\tim\source\repos\JiraCore
+git add .; git commit -m "updated formatting";git push;
+  
+cd C:\users\tim\source\repos\SOMData
+git add .; git commit -m "added code gen";git push;
+
+cd C:\users\tim\source\repos\somuing
+git add .; git commit -m "added support for code gen";git push;
+
+cd C:\users\tim\source\repos\SOM-API
+git add .; git commit -m "added code gen supporting model";git push;
+
+cd C:\users\tim\source\repos\SledgeOMatic;
+git add .; git commit -m "refacted compiler "; git push;
