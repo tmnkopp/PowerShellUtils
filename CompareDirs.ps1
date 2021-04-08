41a0ad459ae6bac31a72a20e9274819cfbd007fc
Tim Kopp <timkopp@gmail.com>
2021-04-08 12:04:43 -0400

    revised diff compare

--- CompareDirs.ps1
+++ CompareDirs.ps1
@@ -2,17 +2,17 @@
 
 $src = 'D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope'
 $dest = 'D:\dev\CyberScope\CyberScope-v-7-34\CSwebdev\code\CyberScope'
-$exc =  @("*vbproj*","*csproj*","*.dll*","*.xml*","*.cache","*.pdb","*.xsd","*.pubxml","*.txt")
-$inc = @("*aspx*","*.vb*","*.ascx")
+$exc =  @("*.resources*","*.config*","*.out*","*.licx*","*\bin*","*\Debug*","*.dll*","*.xml*","*.cache","*.pdb","*.xsd","*.pubxml","*.txt")
+# $inc = @("*aspx*","*.vb*","*.ascx","*.master")
 
-$f1 = Get-ChildItem -Exclude $exc -Include $inc  -Recurse -path $src `
-    | WHERE-OBJECT{ $_.LastWriteTime -gt '4/01/2021' }  
-$f2 = Get-ChildItem -Exclude $exc -Include $inc  -Recurse -path $dest `
-    | WHERE-OBJECT{ $_.LastWriteTime -gt '4/01/2021' } 
+$f1 = Get-ChildItem -Exclude $exc   -Recurse -path $src `
+    | WHERE-OBJECT{ $_.LastWriteTime -gt '3/10/2021' }  
+$f2 = Get-ChildItem -Exclude $exc   -Recurse -path $dest `
+    | WHERE-OBJECT{ $_.LastWriteTime -gt '3/10/2021' } 
 
  
 Compare-Object -ReferenceObject $f1 -DifferenceObject $f2 -Property Name, LastWriteTime, Fullname  `
-    | Where-Object {$_.SideIndicator -eq "<="} `
+    | Where-Object {$_.SideIndicator -eq "<=" -and $_.FullName  -match "\."  } `
     | Out-GridView  -PassThru  | ForEach-Object {  
         $nam = $_.FullName   
         Copy-Item -Path ("$nam")  -Destination $nam.Replace($src, $dest) -Verbose
