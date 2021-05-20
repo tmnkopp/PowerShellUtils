77c317bc18f5deae6890d479cd56d53a866b61cb
Tim Kopp <timkopp@gmail.com>
2021-05-20 09:35:35 -0400

    refactor local

--- GIT.ps1
+++ GIT.ps1
@@ -27,16 +27,16 @@ cd C:\users\tim\source\repos\SOMData
 git add .; git commit -m "added code gen assm";git push;
 
 cd C:\users\tim\source\repos\somuing
-git add .; git commit -m "refactored ";git push;
+git add .; git commit -m "refactored "; git push;
 
 cd C:\users\tim\source\repos\SOM-API
-git add .; git commit -m "added code gen";git push;
+git add .; git commit -m "added code gen"; git push;
  
 cd 'C:\Users\Tim\source\repos\Py\snippets'
 git remote set-url origin   https://github.com/tmnkopp/PythonBoiler.git
 git push --set-upstream origin master
 git remote -v 
-git add .; git commit -m "refac main";git push;
+git add .; git commit -m "refac main"; git push;
 
 
 cd 'D:\PS'
@@ -46,4 +46,4 @@ git remote -v
 git status
 
 cd C:\users\tim\source\repos\JiraFormatter
-git add .; git commit -m "u test revise";git push; 
+git add .; git commit -m "u test revise"; git push; 
