36dda6394f5cb878614fe8f23ff0b57c39a99431
Tim Kopp <timkopp@gmail.com>
2021-05-26 19:42:07 -0400

    refactor local

--- TestAutomator.ps1
+++ TestAutomator.ps1
@@ -1,3 +1,5 @@
+cd 'D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code';  
+dotnet build; 
 
 $exe = [System.Environment]::GetEnvironmentVariable('bom', 'User')   
-& $exe cmd -p cyber.yaml -t 1802
+& $exe cmd -p cyber.yaml -t SOLAR
