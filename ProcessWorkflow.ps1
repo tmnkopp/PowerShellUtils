fae5ce38c4559fdb2fd1814f91e2268715a7d810
Tim Kopp <timkopp@gmail.com>
2021-06-30 15:00:04 -0400

    refactor loc updater

--- ProcessWorkflow.ps1
+++ ProcessWorkflow.ps1
@@ -8,7 +8,6 @@ Stop-process -Name BOM -Force
 $exe = [System.Environment]::GetEnvironmentVariable('bom', 'User')   
 & $exe cmd -t rtime -p timesheets.yaml  
 
-
 Get-Process | Where {$_.ProcessName -Like "BOM"} | Format-Table  
 Get-Process | Where {$_.ProcessName -Like "*chrome*"} | Format-Table  
 
