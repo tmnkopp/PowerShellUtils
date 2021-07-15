e996d30f92c4dc76099551d07181b326c5dd5a2c
Tim Kopp <timkopp@gmail.com>
2021-07-15 10:38:06 -0400

    udpate DB Backerupper

--- backupper.ps1
+++ backupper.ps1
@@ -1,10 +1,14 @@
 explorer.exe D:\Backup\
 explorer.exe C:\SQL\
 
-
+$dt = get-date -format yyyy-MM-ddTHH-mm-ss
+$dt
 $sql = " 
-    BACKUP DATABASE [Cyberscope123] TO  DISK = N'D:\Backup\CyberScope.bak' WITH DIFFERENTIAL, NOFORMAT, NOINIT
-    , NAME = N'Cyberscope-DB Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10 
+     BACKUP DATABASE [Cyberscope123] 
+     TO DISK = N'D:\Backup\CyberScope.bak' 
+     WITH DIFFERENTIAL, NOFORMAT, NOINIT
+    , NAME = N'BK-CS  " + $dt +  "'
+    , SKIP, NOREWIND, NOUNLOAD,  STATS = 10 
 "
 Import-Module  c:\posh\DBUtils.ps1   
 $config = (Get-Content "c:\posh\config.json" -Raw) | ConvertFrom-Json  
@@ -12,3 +16,9 @@ Invoke-Sqlcmd -Database 'Cyberscope123' -Password $config.CSDBPASS -Username CSA
 explorer.exe D:\Backup\
 
 # Invoke-Sqlcmd -Database 'Cyberscope123' -InputFile 'C:\sql\Backup.sql'  -Password $config.CSDBPASS -Username CSAdmin 
+
+
+
+
+
+
