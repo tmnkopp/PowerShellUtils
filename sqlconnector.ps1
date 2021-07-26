4e41950548ee71662ba6be497baf816bca67af53
Tim Kopp <timkopp@gmail.com>
2021-07-26 13:31:14 -0400

    workflow update

--- sqlconnector.ps1
+++ sqlconnector.ps1
@@ -1,12 +1,10 @@
 
- 
 $password = ""  
 $connectionString = ";Initial Catalog=Cyberscope123;Data Source=DESKTOP-OM9UKAC;User ID=timk0pp;Password=$password;Max Pool Size=200"
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionstring
 $connection.Open()
-
 
 $command = New-Object System.Data.SqlClient.SqlCommand
 $command.Connection = $connection
@@ -19,8 +17,6 @@ while ($reader.Read())  { "[0] : " + $reader.GetValue(2)  }
 $reader.Close() 
 # $command.ExecuteNonQuery()
   
-
-
 Get-ChildItem  -Path d:\dev\CyberScope\CyberScopeBranch\CSwebdev\database\ -Recurse -Filter *sql  | `
 Where-Object { ($_.LastWriteTime -gt  (Get-date).AddDays(-5)) } | `
 Where-Object { ($_.FullName -match '(.*)') } | `
