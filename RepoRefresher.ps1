381dc764ae160995ff31cf7e6e9e6dddd2cda1ee
Tim Kopp <timkopp@gmail.com>
2022-01-24 09:59:28 -0500

    unit tests refactor 8487

--- RepoRefresher.ps1
+++ RepoRefresher.ps1
@@ -10,13 +10,17 @@ cd D:\repos\xUnit-Browser-Tests; git add .; git commit -m ("unit tests refactor 
 cd 'c:\posh'; git pull; git add .; git commit -m ("unit tests refactor " + $m) ; git push; 
 cd 'C:\sql'; git add .; git commit -m ("unit tests refactor " + $m) ; git push;
   
-cd C:\users\tim\source\repos\BrowseOmatic; 
-$m = -join ((65..90) + (97..122) | Get-Random -Count 2 | % {$_}); 
+cd C:\users\tim\source\repos\BrowseOmatic;  
 git add .; git commit -m ("unit tests refactor " + $m) ; git push;
 
 cd C:\Users\tim\source\repos\Py\snippets; 
 git pull; git add .; git commit -m ("unit tests refactor " + $m) ; git push;
 
-cd C:\users\tim\source\repos\SledgeOMatic;
-$m = -join ((65..90) + (97..122) | Get-Random -Count 2 | % {$_}); 
+cd C:\users\tim\source\repos\SledgeOMatic; 
 git add .; git commit -m ("compilers refactor "  + $m); git push; 
+ 
+$src = 'D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\Scripts\';cls;  
+$dest = 'D:\dev\CyberScope\CsLab\CSwebdev\code\CyberScope\CyberScope\Scripts\';
+$dt = (Get-date).AddDays(-5) 
+Get-ChildItem "${src}*.*" -File |? { ($_.LastWriteTime -gt $dt)  } |% { Copy-Item $_ $dest} 
+cd 'D:\dev\CyberScope\CsLab\CSwebdev\code\CyberScope'; git add .; git commit -m ("unit tests refactor " + $m); git push; 
