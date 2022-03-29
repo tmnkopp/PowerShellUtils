f5542536c4270d08a4e90f5aa60b221c3056cb3f
Tim Kopp <timkopp@gmail.com>
2022-03-29 15:43:01 -0400

    refac dirs update

--- SVN.ps1
+++ SVN.ps1
@@ -13,14 +13,14 @@ function SVNCommitter
         $stat = $Matches[1] ;  $file = $Matches[2] ; 
         if($stat -match '\?'){  svn add $file;  }     
         if($stat -match 'A|M'){ } # CS-8450    CS-8412 update remove default literal tc not building 
-        svn commit $file -m 'CS-8812 2201 cisa api provider  '; # CS-8494 EINS  CS-8614 CIO
+        svn commit $file -m 'CS-8816 Modify Allow Read Only Insert for batch '; # CS-8494 EINS  CS-8614 CIO
     }    
     cd (((Get-Content "c:\posh\config.json" -Raw) | ConvertFrom-Json).BRANCH   + '\CSwebdev\') # database\
     svn status | Out-GridView -PassThru | ForEach-Object {    
         $_ -match '(.+\s{2,7})(.*)';
         $stat = $Matches[1];  $file = $Matches[2] ;   #  CS-8764  validate apprive remove EO CS-8757  Update Section 2  CSHELP-3160 Adding New BOD 18-02 Remediation Plan CQ Parameter 
         if($stat -match '\?'){  svn add $file;  }      # CS-8764 FY22 FISMA CIO Q2 Data Call CS-8753 justification field requirement update
-        svn commit $file -m ' CS-8816 Modify BOD 22-01 KEV - Section 2 Grid and Validations'; 
+        svn commit $file -m ' CS-8816 Modify Allow Read Only Insert for batch '; 
     }    
     # start chrome https://dayman.cyber-balance.com/TeamCity/project/_Root?mode=builds    
     # CS-8764  validate apprive remove EO 
