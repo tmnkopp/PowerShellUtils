e5a6f5d4f6526883aba9414162ff14c13ec11827
Tim Kopp <timkopp@gmail.com>
2022-06-01 11:22:06 -0400

    Refactor release fixes #97100

--- SVN.ps1
+++ SVN.ps1
@@ -13,7 +13,7 @@ function SVNCommitter
         $stat = $Matches[1] ;  $file = $Matches[2] ; 
         if($stat -match '\?'){  svn add $file;  }     
         if($stat -match 'A|M'){ } # CS-8450    CS-8412 update remove default literal tc not building 
-        svn commit $file -m ' CS-8895 dependancy multi update '; # CS-8494 EINS  CS-8614 CIO
+        svn commit $file -m '  CS-8892 batch Query outputs spreadsheet sheets name limit '; # CS-8494 EINS  CS-8614 CIO
     }    
     $nams = ''; Set-Content -Path 'C:\temp\svnupdates.txt' -Value ''; 
     $base = (((Get-Content "c:\posh\config.json" -Raw) | ConvertFrom-Json).BRANCH   + '\CSwebdev\')
@@ -26,35 +26,25 @@ function SVNCommitter
     }   
     Set-Content -Path 'C:\temp\svnupdates.txt' -Value $nams;  notepad.exe 'C:\temp\svnupdates.txt';
     
-    svn commit --targets 'C:\temp\svnupdates.txt' -m 'CS-8892 add meta data to batch query ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5
+    svn commit --targets 'C:\temp\svnupdates.txt' -m '  CS-8892 batch Query outputs spreadsheet sheets name limit ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5 CS-8895 1b prefix update
     
     # CS-8892 add meta data to batch query
-    # CS-8895 dependancy multi js update
+    # CS-8895 1b prefix update dependancy multi js update 
     svn commit --targets 'C:\temp\svnupdates.txt' -m ' CS-8895 HVA Prepop Orgsubs';  
     svn commit --targets 'C:\temp\svnupdates.txt' -m ' CS-8890 default toggle to hide optional ';   
-    svn commit --targets 'C:\temp\svnupdates.txt' -m ' CS-8885 create data request/response service ';  
-    # IG changes. Logic for the levels
-    # IG warning text changes ass sections
-    # toggle show/hide all pages
-    # formatting 
-    # help desk 
+    svn commit --targets 'C:\temp\svnupdates.txt' -m ' CS-8885 create data request/response service ';   
     # CS-8933 Update 'Show/Hide' Metric Display When Edit/Save is Clicked: FY22 Annual IG
     # CS-8934 Update Show/Hide Metric Toggle Format: FY22 Annual IG
-    # CS-8932 Update Validation Logic/Text- FY22 Annual IG 
-    #
-    #
+    # CS-8932 Update Validation Logic/Text- FY22 Annual IG  
     # CS-8894 1802  
-    # CS-8895 HVA Section 1 DB UPDATE
-    # start chrome https://dayman.cyber-balance.com/TeamCity/project/_Root?mode=builds     
+    # CS-8895 HVA Section 1 DB UPDATE   
     # CS-8901 BOD1802 Annual 1A Db Dependancy Update- 
     # CS-8889 Add Additional Guidance Notes to Each Section: FY22 Annual IG
     # CS-8865 Create Toggle to Show/Hide Optional Metrics: FY22 FISMA Annual IG
     # CS-8883 Update IPv4 Validations - EINSTEIN Config 
     # CS-8816 Modify BOD 22-01 KEV - Section 2 Grid and Validations
-    # CS-8459 Browser Automator IG Config
-    # CS-8816 Modify Allow Read Only Insert for batch   
+    # CS-8459 Browser Automator IG Config   
     # CS-8826 CDM date import - remove date logic
-    # CS-8459 Browser Automator config attribute targets 
-    # CS-8459 Browser Automator 2201 Config
+    # CS-8459 Browser Automator config attribute targets  
     # CSHELP-3211 update section 4 validation
 }
