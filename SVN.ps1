a82ce56878ecab56948dfd18a945459ea49be8a7
Tim Kopp <timkopp@gmail.com>
2022-06-17 15:08:04 -0400

    Refactor release fixes #105118

--- SVN.ps1
+++ SVN.ps1
@@ -26,8 +26,8 @@ function SVNCommitter
     }   
     Set-Content -Path 'C:\temp\svnupdates.txt' -Value $nams;  notepad.exe 'C:\temp\svnupdates.txt';
     # CS-8958 2201 add alias updates to batch 7.44 Checkmarx JavaScript Finding  CS-9008 HtmlEncode
-    # CS-8958 Update export columns
-    svn commit --targets 'C:\temp\svnupdates.txt' -m 'CS-9010 Add htmlencode ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5 CS-8895 1b prefix update
+    # CS-8958 Update export columns  CS-8988 7c Update
+    svn commit --targets 'C:\temp\svnupdates.txt' -m 'CS-9015 q text update ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5 CS-8895 1b prefix update
     # CS-8951 CIOq3 prevent rerun of the post question add form  
     # CS-8892 HVA1802 batch query  
     # CS-8895 HVA Section 1 prefix update dependancy multi js update 
