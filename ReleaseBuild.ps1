700bdacacd38564cdaa8f019fcb84c20b2da85a9
Tim Kopp <timkopp@gmail.com>
2022-06-28 13:16:18 -0400

    Refactor debug fixes #7685

--- ReleaseBuild.ps1
+++ ReleaseBuild.ps1
@@ -5,13 +5,13 @@ function ReleaseBuild
 	(   
         [Alias("s")] 
         [Parameter(Mandatory = $false, Position = 1)] 
-        [string] $Source = 'G:\inetpub\wwwroot\CyberScopeBranch_Release' , 
+        [string] $Source = 'C:\inetpub\wwwroot\CyberScopeBranch_Release' , 
         [Alias("d")] 
         [Parameter(Mandatory = $false, Position = 2)] 
-        [string] $Dest = 'G:\inetpub\wwwroot\CyberScopeBranch',
+        [string] $Dest = 'C:\inetpub\wwwroot\CyberScopeBranch',
         [Alias("t")] 
         [Parameter(Mandatory = $false, Position = 3)] 
-        [string] $Temp = 'C:\temp'  
+        [string] $Temp = 'C:\temp\cs\'  
 	)
     if( $Source -eq ''){
         $Source=($Dest + '_Release')
@@ -20,13 +20,15 @@ function ReleaseBuild
     Write-Verbose $Dest 
     Write-Verbose $Temp  
 
+    Write-Debug 'Copy source to temp?'  
     Get-ChildItem -Path  $Temp | Remove-Item -Recurse -Force   
     robocopy /S /E  $Source  $Temp 
     
     Get-ChildItem -Path  $Temp -Recurse |
         Where-Object FullName -match '.*\\TempUp.*|.*\\TempDown.*|.*\\bin.*|.*\\obj.*|.*Web\.config$' |
         Remove-Item -Recurse -Force; 
-    
+
+    Write-Debug 'Copy to Dest?' 
     Get-ChildItem -Path  $Dest -Recurse |
         Where-Object FullName -notmatch '.*TempUp.*|.*TempDown.*|.*Web\.config$' | 
         Remove-Item -Recurse  -Force; 
@@ -34,22 +36,6 @@ function ReleaseBuild
     robocopy /S /E  $Temp  $Dest
 }     
 
-
-
-Get-ChildItem -Path "D:\dev\CyberScope\temp" | Remove-Item -Recurse -Force   
-robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope D:\dev\CyberScope\temp 
-
-Get-ChildItem -Path "D:\dev\CyberScope\temp" -Recurse |
-    Where-Object FullName -match '.*\\TempUp.*|.*\\TempDown.*|.*\\bin.*|.*\\obj.*|.*Web\.config$' |
-    Remove-Item -Recurse -Force; 
-
-Get-ChildItem -Path "D:\dev\CyberScope\CsLab_Release" -Recurse |
-    Where-Object FullName -notmatch '.*TempUp.*|.*TempDown.*|.*Web\.config$' | 
-    Remove-Item -Recurse  -Force; 
-
-robocopy /S /E D:\dev\CyberScope\temp D:\dev\CyberScope\CsLab_Release
- 
-explorer.exe "D:\dev\CyberScope\temp"
 
 
   
--- RemoveItem.ps1
+++ RemoveItem.ps1
@@ -18,10 +18,7 @@ Where {$_ -match '.*kendo.*|.*CustomControls.*'} |
 Sort-Object -Property CreationTime |
 Write-Output
 
-
-
-
-
+ 
 
 Get-ChildItem -Force -Recurse -File -Path "D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\Scripts\"  `
 | Where-Object { $_.LastWriteTime.Date -gt (Get-Date).AddDays(-60).Date -or $_.CreationTime.Date -gt (Get-Date).AddDays(-60).Date  } `
