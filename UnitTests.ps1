8220febc896ba6fec850ccfa21e75d24b6da25f1
Tim Kopp <timkopp@gmail.com>
2022-06-28 13:40:23 -0400

    Refactor debug fixes #116113

---
+++ UnitTests.ps1
@@ -0,0 +1,8 @@
+cd D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope.Tests\
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.UserMgmt.USER_AGENCY_UPDATES" 
+cd D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope.Tests\
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.DataCall.FormsSubmit" 
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.DataCall.FismaFormValidates" 
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.BOD2201.FormsSubmit"  
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.EINSTEIN.FormsSubmit"  
+dotnet test "bin\debug\CyberScope.Tests.dll" --filter "CyberScope.Tests.Selenium.EINSTEIN.MetricValidates" 
