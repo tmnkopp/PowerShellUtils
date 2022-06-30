de123c1809311b2a8bbdf038349f4f8e4f2c5523
Tim Kopp <timkopp@gmail.com>
2022-06-30 09:44:55 -0400

    Refactor debug fixes #81116

--- SVN.ps1
+++ SVN.ps1
@@ -26,7 +26,8 @@ function SVNCommitter
     }   
     Set-Content -Path 'C:\temp\svnupdates.txt' -Value $nams;  notepad.exe 'C:\temp\svnupdates.txt'; 
     # CS-8958 Update export columns  CS-8988 7c Update CS-9019 null sub in integer
-    svn commit --targets 'C:\temp\svnupdates.txt' -m 'CS-9022 Text update section 1 ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5 CS-8895 1b prefix update
+    # CS-9022 Text update section 1
+    svn commit --targets 'C:\temp\svnupdates.txt' -m 'CS-8648 enable q3 non-eo frm val  ';   #  CS-8894 1802 frmval update CS-8901 BOD1802 Update Sections 4,5 CS-8895 1b prefix update
     # CS-8951 CIOq3 prevent rerun of the post question add form  
     # CS-8892 HVA1802 batch query     
     # CS-8901 BOD1802 Annual 1A Db Dependancy Update-   
