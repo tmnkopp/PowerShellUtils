62b55f35ba4dae6123c2a5301a4cec1c894cda04
Tim Kopp <timkopp@gmail.com>
2023-02-08 08:07:33 -0500

    Refactor debug fixes #6597

--- VersionMerger.ps1
+++ VersionMerger.ps1
@@ -8,11 +8,14 @@ Get-ChildItem -Path "G:\temp" -Recurse |
 
 robocopy /S /E G:\temp G:\inetpub\wwwroot\CyberScopeTrunk ; 
 
-# # #
+# # # 
+#robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev D:\dev\CyberScope\trunk\CSwebdev
 
-robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev D:\dev\CyberScope\trunk\CSwebdev
-robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev D:\dev\CyberScope\trunk\CSwebdev
-
+robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\database D:\dev\CyberScope\trunk\CSwebdev\database
+robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope D:\dev\CyberScope\trunk\CSwebdev\code\CyberScope
+robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope.Automator D:\dev\CyberScope\trunk\CSwebdev\code\CyberScope.Automator
+robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope.Tests D:\dev\CyberScope\trunk\CSwebdev\code\CyberScope.Tests
+robocopy /S /E D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope.Tests.VB D:\dev\CyberScope\trunk\CSwebdev\code\CyberScope.Tests.VB
 explorer.exe "D:\dev\CyberScope\trunk"
 
 cd D:\dev\CyberScope\trunk\; svn status; 
