9037a0f767098690f442f8bc01774f4767c2960a
Tim Kopp <timkopp@gmail.com>
2023-02-14 09:19:43 -0500

    Refactor debug fixes #10477

--- GIT.ps1
+++ GIT.ps1
@@ -4,12 +4,11 @@ cd 'c:\posh'; git pull; git add .; git commit -m ("Refactor debug fixes #"  + $m
 cd C:\Users\timko\source\repos\PythonBoiler;
 git add .; git commit -m "revise unit tests"; git push;   
   
-
-
+ 
 cd 'C:\sql'; git add .; git commit -m ("Refactor DB Bak fixes #"  + $m); git push; 
-cd D:\repos\BrowseOmatic; git pull;  
+cd c:\repos\BrowseOmatic; git pull;  
 git add .; git commit -m ("Compile proj unit test fixes #" + $m) ; git push;  
-cd D:\repos\SledgeOMatic;  git pull;  
+cd c:\repos\SledgeOMatic;  git pull;  
 git add .; git commit -m ("compilers refactor "  + $m); git push;  
 cd C:\Users\Tim\source\repos\Py\snippets; # git status;   
 git pull; git add .; git commit -m ("Compile codegen fixes #"  + $m); git push;  
