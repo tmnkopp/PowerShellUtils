3edf76d87182dce6fab58be38403937ff4b63fdd
Tim Kopp <timkopp@gmail.com>
2024-11-07 09:44:41 -0500

    Refactor debug fixes #7874

--- GIT.ps1
+++ GIT.ps1
@@ -1,7 +1,7 @@
 ﻿$m = -join ((5..90) + (97..122) | Get-Random -Count 2 | % {$_});  
 cd 'c:\posh'; git pull; git add .; git commit -m ("Refactor debug fixes #"  + $m); git push;  
-#cd C:\Users\timko\source\repos\PythonBoiler;
-cd C:\Users\Tim\source\repos\Py\snippets
+cd C:\Users\timko\source\repos\PythonBoiler;
+#cd C:\Users\Tim\source\repos\Py\snippets
 git add .; git commit -m ("refactor jra provider : " + $m); git push;   
 
 robocopy /S /E C:\Users\timko\source\repos\PythonBoiler\CodeGenerators C:\dev\CodeGen\pom
